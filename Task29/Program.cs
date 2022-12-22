450d9efa8cea4b9f74c24994a56c327cdc6a9883
Albina <albamikhaleva@gmail.com>
2022-12-22 19:49:23 +0300

    Задача 29

---
+++ Task29/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+Console.WriteLine ("Введите размер массива:");
+
+int size = int.Parse(Console.ReadLine());
+int [] array = new int [size];
+
+Random random = new Random();
+
+for (int i = 0; i < size; i++) {
+array[i] =  random.Next(0, 100);
+}
+
+PrintArray (array, size);
+
+
+void PrintArray (int [] array, int size)
+{
+for (int i = 0; i < size; i++) {
+    if (i == 0) {
+Console.Write("[" + array[i] + ", ");
+    } else if ((i > 0) & (i < size-1)) {
+        Console.Write(array[i]+ ", ");
+    } else if (i == size-1) {
+        Console.Write(array[i]+ "]");
+    }
+}
+}
