d8e76c607433992cdc1288d42b40a964cd4ab02a
Albina <albamikhaleva@gmail.com>
2022-12-29 18:34:40 +0300

    Задача 45

---
+++ Task45/Program.cs
@@ -0,0 +1,29 @@
+﻿/*  Задача 45: Напишите программу, которая будет создавать копию заданного массива 
+с помощью поэлементного копирования.*/
+
+int [] array = {3, 7, 22, 2, 78};
+//int [] arrayClone = C
+
+Console.WriteLine ("Исходный массив array: ");
+PrintArray (array);
+
+Console.WriteLine ("Копия массива array: ");
+PrintArray (CopyArray(array));
+
+int [] CopyArray (int [] array) {
+    int [] arrayClone =  new int [array.Length];
+
+    for (int i = 0; i < array.Length; i++){
+    arrayClone [i] = array [i];
+    }
+
+    return arrayClone;
+}
+
+void PrintArray (int [] array) {
+for (int i = 0; i < array.Length; i++){
+    Console.Write (array [i] + " ");
+}
+Console.WriteLine();
+
+}
